0023a43f51070aff0d79e9e0bfae6312d93a6d7a
David Mikeladze <davidmikeladze00@gmail.com>
2024-05-29 17:52:47 +0400

    feat: add home page language change

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,8 +1,12 @@
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { Link } from "react-scroll";
 import ScrollReveal from "scrollreveal";
+import { MyContext } from "./Context";
 
 function Home() {
+  const context = useContext(MyContext);
+  const { changeLanguage }: any = context;
+
   // on scroll show component smooth effect
   useEffect(() => {
     const config = {
@@ -25,18 +29,23 @@ function Home() {
     >
       <div className="bg-[rgb(21,22,24,0.5)] text-center rounded-[25px] py-[10px] px-[30px] mb-[15px] lg:mb-[30px] outline-none">
         <h3 className="text-[20px] text-[white] font-light lg:text-[26px]">
-          Welcome to <span className="text-[#E3B81E] font-bold">Grains</span>
+          {changeLanguage === "ENG" ? "Welcome To " : "მოგესალმებით "}
+          <span className="text-[#E3B81E] font-bold">Express Trade</span>
         </h3>
       </div>
-      <h1 className="text-[white] text-[28px] lg:text-[48px] text-center uppercase">
-        We Are Grains Shop
+      <h1 className="text-[white] text-[28px] lg:text-[38px] text-center uppercase">
+        {changeLanguage === "ENG"
+          ? "We Are a Grain Import Company"
+          : "ჩვენ ვართ ბურღულეულის იმპორტიორი კომპანია"}
       </h1>
       <p className="text-[white] text-[14px] mb-[35px] text-center lg:text-[24px]">
-        We are team of talented designers making websites with
+        {changeLanguage === "ENG"
+          ? "In Georgia Azerbaijan and Armenia"
+          : "საქართველოში აზერბაიჯანსა და სომხეთში"}
       </p>
       <Link to={"about"} spy={true} smooth={true} offset={0} duration={500}>
         <button className="bg-[rgb(21,22,24,0.5)] rounded-[25px] py-[8px] px-[30px] text-[white]  outline-none border-[white] hover:bg-[#E3B81E] duration-300 ease-in-out border-[1px] uppercase font-bold">
-          Get Started
+          {changeLanguage === "ENG" ? "Get Started" : "დაწყება"}
         </button>
       </Link>
     </div>
