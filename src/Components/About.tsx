9a3a95c8112e9f1eacdb7523aa0f363b19b63aac
David Mikeladze <davidmikeladze00@gmail.com>
2024-05-31 14:03:31 +0400

    fix: last change

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -32,7 +32,7 @@ function About() {
       </div>
       <div className="flex justify-center items-center mt-[20px] px-[15px]">
         <h1 className="text-[#444444] text-[25px] md:text-[28px] lg:text-[32px] font-bold">
-          {changeLanguage === "ENG" ? "Learn More" : "გაიგეთ მეტი "}
+          {changeLanguage === "ENG" ? "Learn More " : "გაიგეთ მეტი "}
           <span className="text-[#E3B81E]">
             {changeLanguage === "ENG" ? "About Us" : "ჩვენს შესახებ"}
           </span>
